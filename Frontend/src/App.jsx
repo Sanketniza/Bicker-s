f0d70d78342d0caec16be593012e1da5a454c10f
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2024-11-21 16:13:29 +0530

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold text-red-500 underline'>Hello World</h1>  
+      <Button>Click me</Button>
+    </>
+  )
+}
+
+export default App
