b292ad05d703637d76472bc93671553d4dff49c3
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2024-11-21 16:15:08 +0530

    starting one more full stack project

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,8 +7,7 @@ function App() {
 
   return (
     <>
-      <h1 className='text-3xl font-bold text-red-500 underline'>Hello World</h1>  
-      <Button>Click me</Button>
+      <h1>Hello World</h1>
     </>
   )
 }
