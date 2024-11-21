d231871bc1ef90354bbfaf103cf5367b1bfddbbd
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2024-11-21 22:43:45 +0530

    backend: basic backend setup done.

--- Backent/index.js
+++ Backent/index.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const cors = require('cors');
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+const connectDB = require('./utils/db');
 const app = express();
 
-app.use(cors());
+dotenv.config(); // Load environment variables
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+//* Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
+app.use(cookieParser());
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials: true
+};
+
+app.use(cors(corsOptions)); // Enable CORS with options
+
+const PORT = process.env.PORT || 3000;
+
+
+//* database connection
+app.listen(PORT, () => {
+    connectDB();
+    console.log(`Server is running on port ${PORT}`);
+})
