7ad8cf45e2eba63ed1485e40dd3dfce3d6a9f5c6
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-05-14 01:34:12 +0530

    feat: update AdminOrder component layout; enhance styling and structure for improved user experience

--- Frontend/src/components/admin/AdminOrder.jsx
+++ Frontend/src/components/admin/AdminOrder.jsx
@@ -1,13 +1,39 @@
 import AdminNavbar from "../shared/AdminNavbar"
+import Footer from "../shared/footer"
 
 
 function AdminOrder() {
     return (
         <>
             <AdminNavbar />
-            <div>
-                <h1 className="text-4xl font-bold mb-4">Admin Order Page</h1>
+
+            <div className="mx-10">
+                <div className="relative p-10 mx-auto my-10 border rounded-lg shadow-2xl border-orange-500/30 max-w-7xl bg-black/20 backdrop-blur-sm overflow-auto">
+                    <div
+                        className="absolute inset-0 rounded-lg opacity-30 blur-xl"
+                        style={{
+                            background: `radial-gradient(circle at 50% 50%, rgba(16, 185, 129, 0.3), transparent 80%)`,
+                        }}
+                    />
+
+                    <div className="relative z-10">
+                        <h1 className="text-4xl font-bold text-white">
+                            <span className="text-emerald-500">My</span> Orders
+                        </h1>
+
+                        <div className="mt-4 flex justify-between items-center">
+                            <h3 className="text-2xl font-bold text-white">
+                                <span className="text-orange-500">Orders</span> List
+                            </h3>
+                        </div>
+                    </div>
+
+                    
+
+                    </div>
             </div>
+
+            <Footer />
         </>
     )
 }
