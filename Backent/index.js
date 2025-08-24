021b2b593f8403c12033915e9deb1fe0f1a305b4
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-08-25 00:47:04 +0530

    feat: Update CORS configuration to use environment variable for frontend URL and adjust Vercel routing

--- Backent/index.js
+++ Backent/index.js
@@ -27,13 +27,11 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
 app.use(cookieParser());
-const corsOptions = {
-    origin: [
-        "http://localhost:5173",
-        "https://routers-baking-paragraph-sponsors.trycloudflare.com",
-    ],
-    credentials: true
-};
+const frontendURL = process.env.FRONTEND_URL || 'http://localhost:5173';
+    const corsOptions = {
+        origin: frontendURL,
+        credentials:true
+    };
 
 
 app.use(cors(corsOptions)); // Enable CORS with options
